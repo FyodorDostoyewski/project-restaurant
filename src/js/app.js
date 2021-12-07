cf049a91e8b32c49f82bbcfe5cd16876e14e9e5c
Mateusz Górski <mateusz.gorski1302@gmail.com>
2021-12-07 16:21:26 +0100

    changeLetter

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import {settings, select, classNames} from './settings.js';
-import Product from './components/Product.js';
+import Product from './components/product.js';
 import Cart from './components/Cart.js';
-import Booking from './components/Booking.js';
+import Booking from './components/booking.js';
 import Home from './components/Home.js';
 const app = {
   initPages: function(){
